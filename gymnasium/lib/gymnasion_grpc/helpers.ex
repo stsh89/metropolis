29b8f7fd4a128ece0801a1f43da3e33db5b2cd7d
Stanislav Shandyga <stanislavshandyga@gmail.com>
2023-08-03 14:14:56 +0300

    Minor code update

--- gymnasium/lib/gymnasion_grpc/helpers.ex
+++ gymnasium/lib/gymnasion_grpc/helpers.ex
@@ -1,5 +1,5 @@
 defmodule GymnasiumGrpc.Helpers do
-  def to_proto_timestamp(date_time) do
+  def to_proto_timestamp(date_time = %DateTime{}) do
     nanos = DateTime.to_unix(date_time, :nanosecond)
     seconds = DateTime.to_unix(date_time, :second)
 
@@ -9,10 +9,7 @@ defmodule GymnasiumGrpc.Helpers do
     }
   end
 
-  def from_proto_timestamp(%Google.Protobuf.Timestamp{
-        seconds: seconds,
-        nanos: nanos
-      }) do
-    DateTime.from_unix!(seconds * 1_000_000_000 + nanos, :nanosecond)
+  def from_proto_timestamp(timestamp = %Google.Protobuf.Timestamp{}) do
+    DateTime.from_unix!(timestamp.seconds * 1_000_000_000 + timestamp.nanos, :nanosecond)
   end
 end
